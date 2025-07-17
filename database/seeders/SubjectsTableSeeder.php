ce8f8155815eda2fa12bbbca64a96efb0edc55d5
おさる <osaru07m@gmail.com>
2025-07-17 18:18:50 +0900

    feat: 環境構築 #4659 初期データの登録

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Users\Subjects;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,10 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        $subjects = ['国語', '数学', '英語'];
+
+        foreach ($subjects as $subject) {
+            Subjects::create(['subject' => $subject]);
+        }
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,12 @@
 <?php
 namespace Database\Seeders;
 
+use App\Models\Users\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +17,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create('ja-JP');
 
+        // 作成するユーザー数
+        $userCount = 10;
+
+        for ($i = 0; $i < $userCount; $i++) {
+            User::create([
+                'over_name' => 'テスト',
+                'under_name' => "{$i}号",
+                'over_name_kana' => "テスト",
+                'under_name_kana' => "{$i}ゴウ",
+                'mail_address' => "test_{$i}@atlas.jp",
+                // sex => 1: 男, 2: 女
+                'sex' => Arr::random([1, 2]),
+                'birth_day' => $faker->dateTimeBetween('2000-01-01', 'now')->format('Y-m-d'),
+                // role => 1: 教師(国語), 2: 教師(数学), 3: 講師(英語), 4: 生徒
+                'role' => Arr::random([1, 2, 3, 4]),
+                'password' => Hash::make('Compass_1234'),
+            ]);
+        }
     }
 }
