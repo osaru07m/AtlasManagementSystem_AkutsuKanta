a3be6fb8511c9eb27f7b12a78dfe7a0475ae6760
おさる <osaru07m@gmail.com>
2025-07-21 19:18:06 +0900

    fix: 新規投稿作成画面内サブカテゴリーのメインカテゴリーセレクタにデフォルト値を追加する。
    feat: 機能追加（共通） #4678 レイアウトの調整

--- resources/views/authenticated/bulletinboard/post_create.blade.php
+++ resources/views/authenticated/bulletinboard/post_create.blade.php
@@ -3,7 +3,7 @@
   <div class="post_create_area border w-50 m-5 p-5">
     <div class="">
       <p class="mb-0">カテゴリー</p>
-      <select class="w-100" form="postCreate" name="post_category_id">
+      <select class="w-100 form-select" form="postCreate" name="post_category_id">
         @foreach($main_categories as $main_category)
         <optgroup label="{{ $main_category->main_category }}">
         <!-- サブカテゴリー表示 -->
@@ -19,16 +19,16 @@
       <span class="error_message">{{ $errors->first('post_title') }}</span>
       @endif
       <p class="mb-0">タイトル</p>
-      <input type="text" class="w-100" form="postCreate" name="post_title" value="{{ old('post_title') }}">
+      <input type="text" class="w-100 form-control" form="postCreate" name="post_title" value="{{ old('post_title') }}">
     </div>
     <div class="mt-3">
       @if($errors->first('post_body'))
       <span class="error_message">{{ $errors->first('post_body') }}</span>
       @endif
       <p class="mb-0">投稿内容</p>
-      <textarea class="w-100" form="postCreate" name="post_body">{{ old('post_body') }}</textarea>
+      <textarea class="w-100 form-control" form="postCreate" name="post_body">{{ old('post_body') }}</textarea>
     </div>
-    <div class="mt-3 text-right">
+    <div class="mt-3 text-end">
       <input type="submit" class="btn btn-primary" value="投稿" form="postCreate">
     </div>
     <form action="{{ route('post.create') }}" method="post" id="postCreate">{{ csrf_field() }}</form>
@@ -36,12 +36,12 @@
   @can('admin')
   <div class="w-25 ml-auto mr-auto">
     <div class="category_area mt-5 p-5">
-      <div class="">
+      <div class="mb-3">
         @error('main_category_name')
         <span class="error_message">{{ $message }}</span>
         @enderror
         <p class="m-0">メインカテゴリー</p>
-        <input type="text" class="w-100" name="main_category_name" form="mainCategoryRequest">
+        <input type="text" class="w-100 form-control mb-1" name="main_category_name" form="mainCategoryRequest">
         <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="mainCategoryRequest">
       </div>
       <!-- サブカテゴリー追加 -->
@@ -53,12 +53,13 @@
         <span class="error_message">{{ $message }}</span>
         @enderror
         <p class="m-0">サブカテゴリー</p>
-        <select class="w-100" form="subCategoryRequest" name="main_category_id">
+        <select class="w-100 form-select mb-1" form="subCategoryRequest" name="main_category_id">
+          <option value="" disabled selected>---</option>
           @foreach($main_categories as $main_category)
           <option value="{{ $main_category->id }}">{{ $main_category->main_category }}</option>
           @endforeach
         </select>
-        <input type="text" class="w-100" name="sub_category_name" form="subCategoryRequest">
+        <input type="text" class="w-100 form-control mb-1" name="sub_category_name" form="subCategoryRequest">
         <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="subCategoryRequest">
       </div>
       <form action="{{ route('main.category.create') }}" method="post" id="mainCategoryRequest">{{ csrf_field() }}</form>
