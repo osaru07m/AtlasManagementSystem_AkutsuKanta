3cd6c2ffb465b1e5784843f576ec310cb6fac16e
おさる <osaru07m@gmail.com>
2025-07-21 16:21:50 +0900

    feat: 機能追加（スクール予約） #4673 予約をキャンセルをするための確認画面を追加

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,3 +1,14 @@
 $(function(){
-
+    $('.delete-modal-open').on('click', function () {
+        $('.js-modal').fadeIn();
+        var reserve_date = $(this).val();
+        var reserve_part = $(this).text();
+        $('.delete-modal-reserve-date').val(reserve_date);
+        $('.delete-modal-reserve-part').val(reserve_part);
+        return false;
+    });
+    $('.js-modal-close').on('click', function () {
+        $('.js-modal').fadeOut();
+        return false;
+    });
 });
--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -11,6 +11,30 @@
     <div class="text-right w-75 m-auto">
       <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
     </div>
+    <div class="modal js-modal">
+      <div class="modal__bg js-modal-close"></div>
+      <div class="modal__content">
+        <form action="{{ route('deleteParts') }}" method="post">
+          <div class="w-100">
+            <div class="modal-inner-title w-50 m-auto">
+              下記の予約をキャンセルします。よろしいですか？
+            </div>
+            <div class="modal-inner-body w-50 m-auto pt-3 pb-3">
+              <label>予約日時</label>
+              <input class="delete-modal-reserve-date" readonly></input>
+              <label>予約部</label>
+              <input class="delete-modal-reserve-part" readonly></input>
+            </div>
+            <div class="w-50 m-auto edit-modal-btn d-flex">
+              <a class="js-modal-close btn btn-secondary d-inline-block" href="">閉じる</a>
+              <input type="hidden" class="edit-modal-hidden" name="post_id" value="">
+              <input type="submit" class="btn btn-danger d-block" value="キャンセルする">
+            </div>
+          </div>
+          {{ csrf_field() }}
+        </form>
+      </div>
+    </div>
   </div>
 </div>
 </x-sidebar>
